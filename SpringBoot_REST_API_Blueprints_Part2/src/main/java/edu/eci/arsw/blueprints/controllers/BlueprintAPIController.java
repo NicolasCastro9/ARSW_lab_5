873c51fa49537f8822ee18f85667d127e6715b3c
NicolasCastro9 <tankiandown@gmail.com>
2023-09-24 16:00:11 -0500

    1.1 1.2 y 1.3

--- SpringBoot_REST_API_Blueprints_Part2/src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ SpringBoot_REST_API_Blueprints_Part2/src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -8,17 +8,40 @@ package edu.eci.arsw.blueprints.controllers;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.impl.InMemoryBlueprintPersistence;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 
 /**
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
     
-    
+    @Autowired
+    //@Qualifier("Service")
+    BlueprintsServices bps;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getBlueprint(){
+        try{
+            return new ResponseEntity<>(bps.getAllBlueprints(), HttpStatus.ACCEPTED);
+        } catch (BlueprintNotFoundException ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error",HttpStatus.NOT_FOUND);
+        }
+    }
     
     
     
--- SpringBoot_REST_API_Blueprints_Part2/src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
+++ SpringBoot_REST_API_Blueprints_Part2/src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -29,8 +29,14 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
     public InMemoryBlueprintPersistence() {
         //load stub data
         Point[] pts=new Point[]{new Point(140, 140),new Point(115, 115)};
-        Blueprint bp=new Blueprint("_authorname_", "_bpname_ ",pts);
+        Point[] pts2 =new Point[]{new Point(130, 130),new Point(120, 120)};
+        Point[] pts3 =new Point[]{new Point(115, 240),new Point(135, 115)};
+        Blueprint bp=new Blueprint("autor_1", "bps_1",pts);
         blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        Blueprint bp2 =new Blueprint("autor_1", "bps_2",pts2);
+        blueprints.put(new Tuple<>(bp2.getAuthor(),bp2.getName()), bp2);
+        Blueprint bp3 =new Blueprint("autor_2", "bps_3",pts3);
+        blueprints.put(new Tuple<>(bp3.getAuthor(),bp3.getName()), bp3);
         
     }    
     
